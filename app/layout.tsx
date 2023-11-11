dec09abb194784b72d544aa3232e2378e5289316
tonyabracadabra <tongxupeng.cpu@gmail.com>
2023-11-12 01:19:53 +0800

    chore: basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider
@@ -28,5 +29,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    // </ClerkProvider>
   );
 }
