8a6e099ec42f86848e3134985fb1b287fc9c63fd
tonyabracadabra <tongxupeng.cpu@gmail.com>
2023-11-13 14:12:14 +0800

    chore: update

---
+++ meta_analysis/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="meta_analysis",
+    packages=find_packages(exclude=["meta_analysis_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
