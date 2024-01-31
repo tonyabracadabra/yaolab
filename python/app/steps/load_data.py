e4b864880ba64cc08d70e0f56ea528a2e70688b9
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-01-31 15:11:14 +0800

    chore: update config

--- python/app/steps/load_data.py
+++ python/app/steps/load_data.py
@@ -11,13 +11,26 @@ current_dir = os.path.dirname(__file__)
 
 
 def _load_reaction_db(reaction_db: ReactionDatabase) -> pd.DataFrame:
-    reaction_df = load_csv(reaction_db.file)
+    reaction_df: pd.DataFrame = load_csv(reaction_db.file)
 
-    return reaction_df.merge(
-        reaction_db.customReactions or [],
+    # Convert customReactions to DataFrame
+    custom_reactions_df = pd.DataFrame(
+        [reaction.to_dict() for reaction in reaction_db.customReactions]
+    )
+    custom_reactions_df.columns = [
+        "Reaction Description",
+        "Mass Difference(Da)",
+        "Formula Change",
+    ]
+
+    # Merge DataFrames
+    merged_df = reaction_df.merge(
+        custom_reactions_df,
         how="outer",
         on=["Reaction Description", "Mass Difference(Da)", "Formula Change"],
     )
+
+    return merged_df
 
 
 def load_data(
@@ -25,7 +38,7 @@ def load_data(
 ) -> tuple[list[Spectrum], pd.DataFrame, pd.DataFrame]:
     spectra = load_mgf(analysis.rawFile.mgf)
     targeted_ions_df = load_csv(analysis.rawFile.targetedIons)
-    reaction_df = _load_reaction_db(analysis.reactionDb.file)
+    reaction_df = _load_reaction_db(analysis.reactionDb)
 
     input_dir = os.path.join(current_dir, "../input")
     spectra: list[Spectrum] = list(
