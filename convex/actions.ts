a70cf16f41df1ef119024d4f9988fd4e53deb349
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-01-31 15:06:23 +0800

    chore: update config

--- convex/actions.ts
+++ convex/actions.ts
@@ -1,35 +1,8 @@
 import { zid } from "convex-helpers/server/zod";
-import { httpRouter } from "convex/server";
 import { z } from "zod";
 import { internal } from "./_generated/api";
-import { Id } from "./_generated/dataModel";
-import { httpAction } from "./_generated/server";
 import { AnalysisCreationInputSchema } from "./schema";
 import { zAction } from "./utils";
-
-const http = httpRouter();
-
-http.route({
-  path: "/downloadFile",
-  method: "GET",
-  handler: httpAction(async ({ storage }, request) => {
-    const { searchParams } = new URL(request.url);
-    const storageId = searchParams.get("storageId")!;
-    if (!storageId) {
-      return new Response("storageId is required", {
-        status: 400,
-      });
-    }
-
-    const blob = await storage.get(storageId as Id<"_storage">);
-    if (blob === null) {
-      return new Response("File not found", {
-        status: 404,
-      });
-    }
-    return new Response(blob);
-  }),
-});
 
 export const triggerAnalysis = zAction({
   args: { ...AnalysisCreationInputSchema.shape, token: z.string() },
