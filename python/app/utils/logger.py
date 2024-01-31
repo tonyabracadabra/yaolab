58e8ca07f97e12220524237ac4f90013a9cbd32c
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-01-31 16:29:59 +0800

    chore: update config

--- python/app/utils/logger.py
+++ python/app/utils/logger.py
@@ -8,9 +8,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 import logging
 
-# Import your logger here
-from logger import logger
-
 
 def log(message: str):
     def decorator(func):
