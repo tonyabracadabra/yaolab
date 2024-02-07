73628ecdaee759ba7dc7e6a50b82693f5f03235f
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-07 11:48:30 +0800

    chore: refactor

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,17 @@
-import labAnimation from "@/public/animations/lab.json";
+import labLightAnimation from "@/public/animations/lab-black.json";
+import labDarkAnimation from "@/public/animations/lab.json";
+
 import Lottie from "lottie-react";
+import { useTheme } from "next-themes";
 
 export function Logo() {
+  const { theme } = useTheme();
+
   return (
     <div className="w-[50px]">
-      <Lottie animationData={labAnimation} />
+      <Lottie
+        animationData={theme === "dark" ? labDarkAnimation : labLightAnimation}
+      />
     </div>
   );
 }
