1c4439f5be66ff327cb6cd653bcd86b27f73331b
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-08 23:20:41 +0800

    chore: refactor

--- python/app/core/analysis.py
+++ python/app/core/analysis.py
@@ -1,4 +1,5 @@
 import app.core.steps as steps
+import numpy as np
 import pandas as pd
 from app.models.analysis import Analysis, AnalysisStatus
 from app.utils.constants import ID_COL, SAMPLE_COL
@@ -45,6 +46,7 @@ class AnalysisWorker(BaseModel):
         )
         samples_df = targeted_ions_df[SAMPLE_COL]
         targeted_ions_df = targeted_ions_df[""]
+        ids: np.ndarray = targeted_ions_df[ID_COL].values
 
         ion_interaction_matrix: coo_matrix = await create_ion_interaction_matrix(
             targeted_ions_df,
@@ -53,13 +55,14 @@ class AnalysisWorker(BaseModel):
         )
 
         similarity_matrix: coo_matrix = await create_similarity_matrix(
-            spectra, targeted_ions_df
+            spectra,
+            ids,
         )
 
         edge_data_df: pd.DataFrame = await combine_matrices_and_extract_edges(
             ion_interaction_matrix,
             similarity_matrix,
-            ids=targeted_ions_df[ID_COL].values,
+            ids,
             ms2_similarity_threshold=config.ms2SimilarityThreshold,
         )
 
--- python/app/core/steps/create_similarity_matrix.py
+++ python/app/core/steps/create_similarity_matrix.py
@@ -17,17 +17,16 @@ TOLERANCE = 0.005
 
 @log("Creating similarity matrix")
 async def create_similarity_matrix(
-    spectra: List[Spectrum], targeted_ions_df: pd.DataFrame
+    spectra: list[Spectrum], ids: np.ndarray
 ) -> coo_matrix:
-    ids = targeted_ions_df[ID_COL].to_numpy()
     id_to_index = {id_: index for index, id_ in enumerate(ids)}
-
     # Filter spectra and map to indices based on IDs
-    filtered_spectra = []
+    filtered_spectra, filtered_indices = [], []
     for spectrum in spectra:
         spectrum_id = int(spectrum.metadata[SCANS_KEY])
         if spectrum_id in id_to_index:
             filtered_spectra.append(spectrum)
+            filtered_indices.append(id_to_index[spectrum_id])
 
     similarity_measure = ModifiedCosine(tolerance=TOLERANCE)
     cosine_scores: Scores = calculate_scores(
@@ -37,22 +36,12 @@ async def create_similarity_matrix(
         is_symmetric=True,
     )
 
-    rows, cols, data = [], [], []
-    # Populate the similarity_matrix_data with actual scores from cosine_scores
-    for reference, query, score in cosine_scores:
-        ref_id = int(
-            reference.metadata[SCANS_KEY]
-        )  # Assuming the reference ID is stored under 'id' in metadata
-        query_id = int(query.metadata[SCANS_KEY])  # Same assumption for the query ID
+    filtered_indices_array = np.array(filtered_indices)
+    rows = filtered_indices_array[cosine_scores.scores.row]
+    cols = filtered_indices_array[cosine_scores.scores.col]
 
-        if ref_id in id_to_index and query_id in id_to_index:
-            rows.append(id_to_index[ref_id])
-            cols.append(id_to_index[query_id])
-            data.append(score[0])
-
-    # Create the COO sparse matrix directly
     similarity_matrix = coo_matrix(
-        (data, (rows, cols)), shape=(len(targeted_ions_df), len(targeted_ions_df))
+        (cosine_scores.scores.data, (rows, cols)), shape=(len(ids), len(ids))
     )
 
     return similarity_matrix
