7b8d39f67cedd0813dcdf1efe41434f7e0ca1c1b
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-13 11:18:35 +0800

    chore: fix

--- python/app/core/analysis.py
+++ python/app/core/analysis.py
@@ -1,9 +1,9 @@
-import app.core.steps as steps
+from typing import Any, Callable
+
 import numpy as np
 import pandas as pd
 from app.models.analysis import Analysis
-from app.utils.constants import ID_COL, SAMPLE_COL, TARGET_COL, SOURCE_COL
-from app.utils.logger import logger, with_logging_and_context
+from app.utils.constants import ID_COL, SAMPLE_COL, SOURCE_COL, TARGET_COL
 from pydantic import BaseModel
 from scipy.sparse import coo_matrix
 
@@ -15,19 +15,23 @@ class AnalysisWorker(BaseModel):
     convex: ConvexClient
     analysis: Analysis
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        log_and_context = with_logging_and_context(self.convex, self.id)
-        # Apply decorator to all functions in steps module
-        for name in steps.__all__:
-            func = getattr(steps, name, None)
-            if callable(func):
-                decorated_func = log_and_context(func)
-                setattr(steps, name, decorated_func)
-                logger.info(f"Decorated {name} with log_and_context, {decorated_func}")
-
     class Config:
         arbitrary_types_allowed = True
+
+    def _convex_mutation_wrapper(self, step: str, action: str) -> None:
+        mutation = f"analyses:{action}Step"
+        self.convex.mutation(mutation, {"id": self.id, "step": step})
+
+    async def _run_step(self, func: Callable[..., Any], *args, **kwargs) -> Any:
+        """
+        Executes a step function with the provided arguments, automatically inferring the step name from the function name.
+        Handles the Convex mutations for step start and completion.
+        """
+        step_name = func.__name__
+        self._convex_mutation_wrapper(step_name, "start")
+        result = await func(*args, **kwargs)
+        self._convex_mutation_wrapper(step_name, "complete")
+        return result
 
     async def run(self) -> None:
         from app.core.steps import (
@@ -41,42 +45,62 @@ class AnalysisWorker(BaseModel):
         )
 
         config = self.analysis.config
-        spectra, targeted_ions_df, reaction_df = await load_data(
-            self.analysis, convex=self.convex
+
+        spectra, targeted_ions_df, reaction_df = await self._run_step(
+            load_data, self.analysis, convex=self.convex
         )
+
         samples_df = targeted_ions_df[SAMPLE_COL]
         targeted_ions_df = targeted_ions_df[""]
         ids: np.ndarray = targeted_ions_df[ID_COL].values
 
-        ion_interaction_matrix: coo_matrix = await create_ion_interaction_matrix(
+        ion_interaction_matrix: coo_matrix = await self._run_step(
+            create_ion_interaction_matrix,
             targeted_ions_df,
             reaction_df,
-            mz_error_threshold=self.analysis.config.mzErrorThreshold,
+            mz_error_threshold=config.mzErrorThreshold,
         )
 
-        similarity_matrix: coo_matrix = await create_similarity_matrix(
+        similarity_matrix: coo_matrix = await self._run_step(
+            create_similarity_matrix,
             spectra,
             ids,
         )
 
-        edges: pd.DataFrame = await combine_matrices_and_extract_edges(
+        edges: pd.DataFrame = await self._run_step(
+            combine_matrices_and_extract_edges,
             ion_interaction_matrix,
             similarity_matrix,
             ids,
             ms2_similarity_threshold=config.ms2SimilarityThreshold,
         )
 
-        await calculate_edge_metrics(samples_df, targeted_ions_df, edges)
+        await self._run_step(
+            calculate_edge_metrics,
+            samples_df,
+            targeted_ions_df,
+            edges,
+        )
 
-        await edge_value_matching(
+        await self._run_step(
+            edge_value_matching,
             edges,
             reaction_df,
             rt_time_window=config.rtTimeWindow,
             mz_error_threshold=config.mzErrorThreshold,
             correlation_threshold=config.correlationThreshold,
         )
+
         nodes: pd.DataFrame = pd.concat([targeted_ions_df, samples_df], axis=1)
         # filter out nodes that are not in the edges
-        nodes = nodes[nodes[ID_COL].isin(edges[[TARGET_COL, SOURCE_COL]].values.flatten())]
+        nodes = nodes[
+            nodes[ID_COL].isin(edges[[TARGET_COL, SOURCE_COL]].values.flatten())
+        ]
 
+        self.convex.mutation(
+            "analyses:startStep", {"id": self.id, "step": "upload_result"}
+        )
         await upload_result(id=self.id, nodes=nodes, edges=edges, convex=self.convex)
+        self.convex.mutation(
+            "analyses:completeStep", {"id": self.id, "step": "upload_result"}
+        )
--- python/app/utils/logger.py
+++ python/app/utils/logger.py
@@ -1,10 +1,6 @@
 import functools
 import logging
 
-from app.models.analysis import AnalysisStatus
-
-from convex import ConvexClient
-
 # Configure your logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,40 +15,5 @@ def log(message: str):
             return await func(*args, **kwargs)
 
         return wrapper
-
-    return decorator
-
-
-def with_logging_and_context(convex: ConvexClient, analysis_id: str):
-    def decorator(func: callable):
-        @functools.wraps(func)
-        async def wrapped(*args, **kwargs):
-            step = func.__name__
-            log_message = getattr(func, "log_message", f"Starting '{func.__name__}'")
-            try:
-                logger.info(f"start step {log_message} for analysis {analysis_id}")
-                convex.mutation("analyses:startStep", {"id": analysis_id, "step": step})
-                result = await func(*args, **kwargs)
-                convex.mutation(
-                    "analyses:completeStep", {"id": analysis_id, "step": step}
-                )
-
-                return result
-            except Exception as e:
-                logger.error(
-                    f"Error occurred while executing '{func.__name__}' for analysis {analysis_id}"
-                )
-                convex.mutation(
-                    "analyses:update",
-                    {
-                        "id": analysis_id,
-                        "status": AnalysisStatus.FAILED,
-                        "log": str(e),
-                    },
-                )
-                # Handle exception logging
-                raise e
-
-        return wrapped
 
     return decorator
