d50ae65511feacf925f000c28dda87ca1f63bcc3
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-14 15:52:49 +0800

    chore: add legend

--- app/dashboard/analysis/[id]/page.tsx
+++ app/dashboard/analysis/[id]/page.tsx
@@ -634,11 +634,11 @@ export default function Page({ params }: { params: { id: Id<"analyses"> } }) {
                       />
                       {/* legend for ratios */}
                       {nodeType === "ratio" && (
-                        <div className="flex items-center justify-center gap-2 w-8 h-8 flex-col left-[30px] top-[50px] absolute">
+                        <div className="flex items-center justify-center gap-2 w-18 flex-col left-[30px] top-[60px] absolute">
                           {analysis.config.experiments.map((e, i) => (
                             <div
                               key={i}
-                              className="flex items-center justify-center gap-2"
+                              className="flex items-center justify-center gap-2 w-full"
                             >
                               <div
                                 className="w-4 h-4"
