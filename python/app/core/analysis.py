4c7b5119efea95ba7bd23509589e28c705ff3c66
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-18 02:11:21 +0800

    chore: further filtering

--- python/app/core/analysis.py
+++ python/app/core/analysis.py
@@ -99,7 +99,10 @@ class AnalysisWorker(BaseModel):
             )
         ]
 
-        exps = [e.name for e in config.bioSamples] + [config.drugSample.name]
+        exps = [e.name for e in config.bioSamples]
+        if config.drugSample:
+            exps.append(config.drugSample.name)
+
         ratios = [exp + "_ratio" for exp in exps]
         nodes[ratios] = nodes[exps].div(nodes[exps].sum(axis=1), axis=0)
 
