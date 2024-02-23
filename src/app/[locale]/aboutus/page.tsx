4b62591e761b7883acfd4c19d5a0ee6eb5ffc371
tonyabracadabra <tongxupeng.cpu@gmail.com>
2024-02-23 17:38:12 +0800

    chore: update

--- src/app/[locale]/aboutus/page.tsx
+++ src/app/[locale]/aboutus/page.tsx
@@ -92,7 +92,7 @@ export default function Aboutus() {
           )}
         />
       </div>
-      <MagicContainer className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <MagicContainer className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {people.map((person, index) => (
           <MagicCard key={index} className="flex flex-col items-start gap-4">
             <Image
